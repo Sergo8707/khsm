167d32ec1252a67b53827522f3e01f8ef8aea22e
sergo <sergo8707@gmail.com>
2016-06-30 13:51:05 +0300

    add test models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610145015) do
+ActiveRecord::Schema.define(version: 20160630072622) do
 
   create_table "game_questions", force: :cascade do |t|
     t.integer  "game_id"
@@ -67,5 +67,11 @@ ActiveRecord::Schema.define(version: 20160610145015) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "widgets", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
